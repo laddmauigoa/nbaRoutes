6f2ecda0ea24bbb52e9b69a4bbc373c7b56d9e2a
LaddMauigoa <laddmauigoa@gmail.com>
2015-01-28 14:34:59 -0700

    done except for step 6

--- js/teams/teamService.js
+++ js/teams/teamService.js
@@ -2,4 +2,53 @@ var app = angular.module('nbaRoutes');
 
 app.service('teamService', function($http, $q){
 
+	this.addNewGame = function(gameObj) {
+		var url = "https://api.parse.com/1/classes/" + gameObj.homeTeam;
+		if(parseInt(gameObj.homeTeamScore) > parseInt(gameObj.OpponentScore))  {
+				
+				gameObj.won = true;
+			} 
+			else{
+				gameObj.won = false;
+			}
+				return $http({
+					method: 'POST',
+					url: url,
+					data: gameObj
+				})
+	} 
+
+
+	this.getTeamData = function(team) {
+		var dfd = $q.defer();
+		var url = 'https://api.parse.com/1/classes/' + team;
+
+		$http({
+			method: 'GET',
+			url: url
+
+		}).then(function(data) {
+			var results = data.data.results;
+			var wins = 0;
+			var losses = 0;
+			for(i = 0; i < results.length; i++) {
+				if(results[i].won === true) {
+					wins++;
+				} 
+				else if(results[i].won === false) {
+					losses++;
+				}
+			}
+			results['wins'] = wins;
+			results['losses'] = losses;
+			dfd.resolve(results);
+			
+		})
+
+
+
+
+		return dfd.promise
+	}
+	// getTeamData();
 });
